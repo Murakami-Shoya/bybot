0c7b62b9fbc4697e4b5c56b699eb4d6630e283df
murakami <murashoya@gmail.com>
2022-11-10 23:22:58 +0900

    [add] apiの設定
    settings.iniでapiの設定を追加。個人情報を含むため、以降はコミットしない。

--- opt/main.py
+++ opt/main.py
@@ -1,1 +1,11 @@
-# main.py
+import logging
+import sys
+
+import settings
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+if __name__ == "__main__":
+    print(settings.api_key)
+    print(settings.api_secret)
+    
